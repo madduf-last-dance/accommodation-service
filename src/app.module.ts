e047628171d6d3e91a817a0ae63a3a3d5e3ca7c5
Nikola Petrovic <s.nino.petrovic@gmail.com>
2024-04-25 19:15:47 +0200

    feat: added config for env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,14 @@ import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: [".env.dev", ".env"],
+    }),
     AccommodationModule,
     TypeOrmModule.forRoot({
       type: "postgres",
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_HOST),
+      port: parseInt(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
